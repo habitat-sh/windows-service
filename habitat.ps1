bfa80b232286bfae00e2b7801d162ebac272290c
mwrock <matt@mattwrock.com>
2017-10-23 17:31:15 -0700

    adding installer and uninstaller
    Signed-off-by: mwrock <matt@mattwrock.com>

---
+++ habitat.ps1
@@ -0,0 +1,43 @@
+﻿function Install-HabService {
+	if((Get-Service Habitat -ErrorAction SilentlyContinue) -ne $null) {
+		Write-Error "The Habitat service is already installed. Please run 'hab exec core/windows-service uninstall' first if you wish to reinstall."
+		return
+	}
+
+	$svcPath = Join-Path $env:SystemDrive "hab\svc\windows-service"
+	if(!(Test-Path $svcPath)) {
+		mkdir $svcPath
+	}
+
+	Copy-Item "$PSScriptRoot\*" $svcPath -Force
+
+	&$env:systemroot\system32\sc.exe create Habitat binpath= "$svcPath\HabService.exe" DisplayName= "The Habitat Supervisor service" start= auto
+	if($LASTEXITCODE -ne 0) {
+	    Write-Error "Failed to install the Habitat Service!"
+	}
+	else {
+		Write-Host "Congratulations! The Habitat Service has succesfully been installed!"
+	}
+}
+
+function Uninstall-HabService {
+	if((Get-Service Habitat -ErrorAction SilentlyContinue) -eq $null) {
+		Write-Error "The Habitat service is not installed."
+		return
+	}
+
+	$svcPath = $env:SystemDrive -join "hab\svc\windows-service"
+	Stop-Service Habitat
+	while((Get-Service Habitat).Status -ne "Stopped") {
+		Start-Sleep -Seconds 1
+	}
+
+	&$env:systemroot\system32\sc.exe delete Habitat
+
+	if($LASTEXITCODE -ne 0) {
+	    Write-Error "Failed to uninstall the Habitat Service!"
+	}
+	else {
+		Write-Host "The Habitat Service has succesfully been uninstalled!"
+	}
+}
--- plan.ps1
+++ plan.ps1
@@ -5,7 +5,7 @@ $pkg_source="nosuchfile.tar.gz"
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_license=@('Apache-2.0')
 $pkg_description="A Windows Service for runnung the Habitat Supervisor"
-$pkg_deps=@("core/hab-launcher")
+$pkg_deps=@("core/hab-launcher", "core/powershell")
 $pkg_build_deps=@("core/nuget")
 $pkg_bin_dirs=@("bin")
 
@@ -25,4 +25,5 @@ function Invoke-Build {
 
 function Invoke-Install {
   Copy-Item $HAB_CACHE_SRC_PATH/$pkg_dirname/bin/release/* $pkg_prefix/bin
+  Copy-Item $HAB_CACHE_SRC_PATH/$pkg_dirname/* $pkg_prefix/bin -Include @("*.bat", "habitat.ps1")
 }
