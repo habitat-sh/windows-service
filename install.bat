5ac3de035db05e60797d1fac4ae394fcae17c5c7
mwrock <matt@mattwrock.com>
2018-02-23 10:29:47 -0800

    make compatible with powershell 6.0 RTM
    Signed-off-by: mwrock <matt@mattwrock.com>

--- install.bat
+++ install.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell.exe -NoProfile -ExecutionPolicy bypass -NoLogo -Command ". '%~dp0habitat.ps1';Install-HabService" %*
+pwsh.exe -NoProfile -ExecutionPolicy bypass -NoLogo -Command ". '%~dp0habitat.ps1';Install-HabService" %*
